f8766f9abf41df01246c194c744e1892b2b1a9c8
git-hkj <76410061+git-hkj@users.noreply.github.com>
2023-02-17 15:58:53 +0100

    Input System and Key binding used

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private InputAction movement;
+    [SerializeField] private InputAction fire;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    //To activate 
+    void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    //To deactivate
+    void OnDisable()
+    {
+        movement.Disable();
+    }
     // Update is called once per frame
     void Update()
     {
-        float horizontalPush = Input.GetAxis("Horizontal");
-        Debug.Log(horizontalPush);
-
-        float verticalThrow = Input.GetAxis("Vertical");
-        Debug.Log(verticalThrow);
+        float horizontalPush = movement.ReadValue<Vector2>().x;
+        float verticalThrow  = movement.ReadValue<Vector2>().y;
+       
     }
 }
