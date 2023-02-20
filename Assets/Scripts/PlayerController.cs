ef616b3864511bea62e5f91b1e81f83eae7ea142
git-hkj <76410061+git-hkj@users.noreply.github.com>
2023-02-20 08:47:31 +0100

    Start Player movement
    Also removed the Input System method

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,35 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private InputAction movement;
-    [SerializeField] private InputAction fire;
-
-    // Start is called before the first frame update
+   // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    //To activate 
-    void OnEnable()
-    {
-        movement.Enable();
-    }
-
-    //To deactivate
-    void OnDisable()
-    {
-        movement.Disable();
-    }
-    // Update is called once per frame
+   // Update is called once per frame
     void Update()
     {
-        float horizontalPush = movement.ReadValue<Vector2>().x;
-        float verticalThrow  = movement.ReadValue<Vector2>().y;
-       
+        float horizontalPush = Input.GetAxis("Horizontal");
+        float verticalThrow = Input.GetAxis("Vertical");
+
+        float xDelta = 0.01f;
+        float newXpos = transform.localPosition.x + xDelta;
+
+
+        transform.localPosition = new Vector3(newXpos, transform.localPosition.y, transform.localPosition.z);
+
     }
 }
