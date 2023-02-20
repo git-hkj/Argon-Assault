739b55625eb5bd3b00e6bc164dce61beb102d66e
git-hkj <76410061+git-hkj@users.noreply.github.com>
2023-02-20 13:29:57 +0100

    de/-activating particle emission

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,11 +12,14 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private float xRange = 5f;
     [SerializeField] private float yRange = 1.0f;
+    [SerializeField] private GameObject[] lasers;
 
     [SerializeField] private float pitchFactor = 2.0f;
     [SerializeField] private float controlPitchFactor = -10.0f;
     [SerializeField] private float yawFactor = 2.0f;
     [SerializeField] private float controlRollFactor = -10.0f;
+
+    [SerializeField] private InputAction fireLaser;     //for new system, Input Manager
 
     private float verticalThrow;
     private float horizontalPush;
@@ -30,6 +36,7 @@ public class PlayerController : MonoBehaviour
         
         ProcessTranslation();
         ProcessRotation();
+        ProcessFiring();
     }
 
     //To process a translating movement of the ship using input keys
@@ -65,4 +72,41 @@ public class PlayerController : MonoBehaviour
         
         transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
+
+    //To process the laser firing whenever engaged
+    void ProcessFiring()
+    {
+        // if(fire.ReadValue<float>() > 0.5f) for new input system also need OnEnable() and Disable()
+
+        if (Input.GetButton("Fire1"))
+        {
+            ActivateLaser();
+        }
+        else
+        {
+            DeactivateLaser();
+        }
+    }
+
+    //To switch on the laser whenever the keys are engaged
+    private void ActivateLaser()
+    {
+        foreach (GameObject barrel in lasers)
+        {
+            var emissionModule = barrel.GetComponent<ParticleSystem.EmissionModule>();
+            emissionModule.enabled = true;
+        }
+    }
+
+    //To switch off the laser once the keys are released
+    private void DeactivateLaser()
+    {
+        foreach (GameObject barrel in lasers)
+        {
+            var emissionModule = barrel.GetComponent<ParticleSystem.EmissionModule>();
+            emissionModule.enabled = false;
+        }
+    }
+
+    
 }
