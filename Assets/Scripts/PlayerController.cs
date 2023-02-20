78af67d1378c3600558ce8f05f8c7aad4f18d8a3
git-hkj <76410061+git-hkj@users.noreply.github.com>
2023-02-20 10:25:51 +0100

    Rotation implemented
    wrt pitch, yaw and roll

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float controlSpeed = 10.0f;
+
     [SerializeField] private float xRange = 5f;
-    [SerializeField] float yRange = 1.0f;
+    [SerializeField] private float yRange = 1.0f;
+
+    [SerializeField] private float pitchFactor = 2.0f;
+    [SerializeField] private float controlPitchFactor = -10.0f;
+    [SerializeField] private float yawFactor = 2.0f;
+    [SerializeField] private float controlRollFactor = -10.0f;
+
+    private float verticalThrow;
+    private float horizontalPush;
 
     // Start is called before the first frame update
     void Start()
@@ -18,19 +27,42 @@ public class PlayerController : MonoBehaviour
    // Update is called once per frame
     void Update()
     {
-        float horizontalPush = Input.GetAxis("Horizontal");
-        float verticalThrow  = Input.GetAxis("Vertical");
+        
+        ProcessTranslation();
+        ProcessRotation();
+    }
 
-        float xDelta  = horizontalPush * Time.deltaTime *controlSpeed;
+    //To process a translating movement of the ship using input keys
+    private void ProcessTranslation()
+    {
+        horizontalPush = Input.GetAxis("Horizontal");
+        verticalThrow = Input.GetAxis("Vertical");
+
+        float xDelta = horizontalPush * Time.deltaTime * controlSpeed;
         float rawXPos = transform.localPosition.x + xDelta;
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
-        float yDelta  = verticalThrow * Time.deltaTime * controlSpeed;
+        float yDelta = verticalThrow * Time.deltaTime * controlSpeed;
         float rawYPos = transform.localPosition.y + yDelta;
         float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
 
+    //To process a rotating movement of the ship using input keys
+    private void ProcessRotation()
+    {
+
+        //pitch due to local position and control
+        float pitchDueToPosition = transform.localPosition.y * pitchFactor;
+        float pitchDueToControl = verticalThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControl;
+        //yaw due to local position
+        float yaw = transform.localPosition.x *yawFactor;
+        //roll due to control
+        float roll  = horizontalPush * controlRollFactor;
+        
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
