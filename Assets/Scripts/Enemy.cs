845913881ecffa4d567da254fc33e0ee89e73735
git-hkj <76410061+git-hkj@users.noreply.github.com>
2023-02-21 08:48:36 +0100

    Enemy explosion instantiated

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private GameObject deathVFX;
+    [SerializeField] Transform parent;
+
     void OnParticleCollision(GameObject other)
     {
         Debug.Log($"I'm hit by {other.gameObject.name}");
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
