4ad9fa9c96414e00b8af45d993d1451faab3514e
git-hkj <76410061+git-hkj@users.noreply.github.com>
2023-02-21 12:23:37 +0100

    Score Display
    Used ToString() to convert

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,26 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreBoard : MonoBehaviour
 {
     int score =0;
-
-    //To keep track of the score
-    public void ScoreUpdate(int scoreInc)
-    {
-        score += scoreInc;
-        Debug.Log($"Current Score:{score}");
-    }
+    TMP_Text scoreText;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "Welcome";
     }
 
     // Update is called once per frame
     void Update()
     {
    
+    }
+
+    //To keep track of the score
+    public void ScoreUpdate(int scoreInc)
+    {
+        score += scoreInc;
+        Debug.Log($"Current Score:{score}");
+        scoreText.text = "Score:";
+        scoreText.text += score.ToString();
     }
 }
